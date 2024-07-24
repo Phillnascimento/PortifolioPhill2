06ece067b38d9ba00c99a649b988d71e0d1902d7
Phillnascimento <phill.nascimento.art@gmail.com>
2024-07-23 22:56:41 -0300

    Atualiza Container para aceitar className

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,6 +1,13 @@
-export default function Container({ children }) {
+// components/Container.tsx
+
+type ContainerProps = {
+  children: React.ReactNode;
+  className?: string; // Adicione essa linha para aceitar a prop className
+};
+
+export default function Container({ children, className = '' }: ContainerProps) {
   return (
-    <div className="max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8">
+    <div className={`max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8 ${className}`}>
       {children}
     </div>
   );
